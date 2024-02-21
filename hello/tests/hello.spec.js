c81a7e46272118e8452581fa0289b9776192b30e
up1 <somkiat.p@gmail.com>
2024-02-21 16:16:28 +0700

    Working with test id and mock network

--- hello/tests/hello.spec.js
+++ hello/tests/hello.spec.js
@@ -1,13 +1,37 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Hello XXX', async ({ page }) => {
+test('Hello @mock', async ({ page }) => {
+  // Arrange  
+  // Mock api = GET https://demo-backend-nodejs.vercel.app/
+  await page.route('https://demo-backend-nodejs.vercel.app/', async route => {
+    const response = {
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({ message: 'My Hello' }),
+    };
+    await route.fulfill(response);
+  });
+
   await page.goto('https://demo-frontend-reactjs.vercel.app/');
 
   // Expect a title "to contain" a substring.
 
   await expect(page.getByTestId('message_text')).toHaveText('Call REST API');
-  
+
+  const helloText = page.getByTestId('hello_text');
+  await expect(helloText).toHaveText('My Hello');
+});
+
+test('Hello @real', async ({ page }) => {
+  await page.goto('https://demo-frontend-reactjs.vercel.app/');
+
+  // Expect a title "to contain" a substring.
+
+  await expect(page.getByTestId('message_text')).toHaveText('Call REST API');
+
   const helloText = page.getByTestId('hello_text');
   await expect(helloText).toHaveText('Hello World!');
 });
+
+
