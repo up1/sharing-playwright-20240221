547f859773b509c613df61ae4b6df8bba1389de3
up1 <somkiat.p@gmail.com>
2024-02-21 11:24:14 +0700

    Enable vdo

--- hello/tests/example.spec.js
+++ hello/tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
+test('has title @group1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
