541263b5455d0b2f7cbacbc51af16c77434607b4
up1 <somkiat.p@gmail.com>
2024-02-21 15:17:41 +0700

    Add test step

--- hello/tests/google.spec.js
+++ hello/tests/google.spec.js
@@ -2,24 +2,31 @@
 const { test, expect } = require('@playwright/test');
 
 test('ผู้ใช้งานทำการค้นหาคำว่าหวย แล้วจะต้องเจอผลการค้นหาตาม req', async ({ page }) => {
-  await page.goto('https://www.google.com/');
+  // Step 1 :: go to search page.
+  await test.step('Step 1 :: go to search page', async () => {
+    await page.goto('https://www.google.com/');
+  });
 
-  // Type into search box.
-  const seachField = page.locator('#APjFqb');
-  await seachField.fill('หวย');
-  await seachField.press('Enter');
+  await test.step('Step 2 :: Type into search box', async () => {
+    const seachField = page.locator('#APjFqb');
+    await seachField.fill('หวย');
+    await seachField.press('Enter');
+  });
 
-  // Assert "ผลการค้นหาประมาณ 101,000,000 รายการ (0.36 วินาที)"
-  // Assert text in id=result-stats
-  await expect(page.locator('#result-stats')).toHaveText(/ผลการค้นหาประมาณ/);
-  await expect(page.locator('#result-stats')).toHaveText(/รายการ/);
-  await expect(page.locator('#result-stats')).toHaveText(/วินาที/);
+  await test.step('Step 3 :: Assert text in id=result-stats', async () => {
+    // Assert "ผลการค้นหาประมาณ 101,000,000 รายการ (0.36 วินาที)"
+    // Step 3 :: Assert text in id=result-stats
+    await expect(page.locator('#result-stats')).toHaveText(/ผลการค้นหาประมาณ/);
+    await expect(page.locator('#result-stats')).toHaveText(/รายการ/);
+    await expect(page.locator('#result-stats')).toHaveText(/วินาที/);
 
-  // Assert with regular expression ผลการค้นหาประมาณ 101,000,000 รายการ (0.36 วินาที)
-  await expect(page.locator('#result-stats'))
-  .toHaveText(/ผลการค้นหาประมาณ \d{1,3},\d{3},\d{3} รายการ \(\d{1,2}\.\d{1,2} วินาที\)/);
+    // Assert with regular expression ผลการค้นหาประมาณ 101,000,000 รายการ (0.36 วินาที)
+    await expect(page.locator('#result-stats'))
+      .toHaveText(/ผลการค้นหาประมาณ \d{1,3},\d{3},\d{3} รายการ \(\d{1,2}\.\d{1,2} วินาที\)/);
+
+  });
 
 
-  
+
 });
 
